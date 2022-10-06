eec77a95eaf4d2ff57834ba89570eb693c7c561e
Frederico Jordan <fredericojordan@gmail.com>
2022-10-06 11:16:09 -0300

    Minor refactor

--- corr/corr.py
+++ corr/corr.py
@@ -1,17 +1,17 @@
-import datetime as dt
+import datetime
 
-import matplotlib.pyplot as plt
-import seaborn as sns
-from pandas_datareader import data as pdr
+import seaborn
+from matplotlib.pyplot import show
+from pandas_datareader.data import get_data_yahoo
 
-now = dt.datetime.now()
-start = now - dt.timedelta(days=365)
+now = datetime.datetime.now()
+start = now - datetime.timedelta(days=365)
 
-price = pdr.get_data_yahoo(
-    ["PETR4.SA", "VALE3.SA", "WEGE3.SA", "LREN3.SA"], start, now
-)["Adj Close"]
+price = get_data_yahoo(["PETR4.SA", "VALE3.SA", "WEGE3.SA", "LREN3.SA"], start, now)[
+    "Adj Close"
+]
 returns = price.pct_change().dropna()
 corr = returns.corr()
 
-plot = sns.heatmap(corr, annot=True, fmt=".2f", linewidths=0.6)
-plt.show()
+plot = seaborn.heatmap(corr, annot=True, fmt=".2f", linewidths=0.6)
+show()
